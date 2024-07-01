d767dd6c5c1b9b8cc822ee81f6961607785ee1fa
Titou-Dh <titoudh1@gmail.com>
2024-07-01 01:12:41 +0100

    renamed photos and added the navbar

--- app/page.js
+++ app/page.js
@@ -1,5 +1,7 @@
 import Image from "next/image";
 
+
+
 export default function Home() {
   return (
     <main className="min-h-screen overflow-x-hidden  relative px-24">
@@ -12,7 +14,7 @@ export default function Home() {
       <div class="absolute inset-0 w-screen top-80   flex items-center left-1/4  justify-center">
         <div class="bg-main w-52 h-52 rounded-full filter blur-3xl"></div>
       </div>
-      <div class="relative z-10 text-center">
+      <div class="relative z-10 ">
         
       </div>
     </main>
